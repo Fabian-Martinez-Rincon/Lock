1e4b313bca6bd4a316f62c8cb476d8e31003cc92
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-05-29 14:45:23 -0300

    Prueba2

--- Programa/ARDUINO/ARDUINO.ino
+++ Programa/ARDUINO/ARDUINO.ino
@@ -1,10 +1,14 @@
                  /*Pin 1, 2, 3 son salidas (leds). Pin 5, 6, 7 botones del ascensor. Pin 8 Baja . Pin 9 Sube  */
-                //Empiezo en el piso Pb
+                       
+/*                
+int pb=2;
+int p1=3;
+int p2=4*/
 int Baja=8;
 int Sube=9;
 int botones[3]={5,6,7};
 int plantas[3]={2, 3, 4};
-int almacenar;
+int almacenar[3];
 int i;
 int recargar;
 void setup() {
@@ -24,42 +28,55 @@ void setup() {
 
 void loop() {
   agregarPlanta();
-  revisarPulsadores();
+  //revisarPulsadores();
+
   fijarRecorrido();
 
 }
+void fijarRecorrido(){
+  if (digitalRead (botones[0])==1){
+    digitalWrite(plantas[0],HIGH);
+  }
+  //else digitalWrite(plantas[0],LOW);
+ if (botones[1]==1){
+    digitalWrite(plantas[1],HIGH);
+ }
+ //else{digitalWrite(plantas[1],LOW);}
+ if (botones[2]==1){
+    digitalWrite(plantas[2],HIGH);
+ }
+ //else{digitalWrite(plantas[2],LOW);}
+  
+  }
+
+
+
+
+
+
+
+
+
 void agregarPlanta(){}
 
-
+/*
 void revisarPulsadores(){
 
-  for( i=0; i<3; i ++){
+ 
   
-   if(digitalRead(botones[0])==1){
-    plantas[i]=2;
-    i++;
+    if(digitalRead(botones[0])==1){
+      almacenar[i]=almacenar[2];
+      
     }
-  if (digitalRead[1]==1){
+    if (digitalRead[1]==1){
 
-    plantas[i]=3;
-    i++;;
+      almacenar[i]=almacenar[3];
+      
     }
-  if (digitalRead[2]==1){
+    if (digitalRead[2]==1){
 
-    plantas[i]=4;
-    i++;
-    }
-  }
-  }
-
-
-void fijarRecorrido(){
-  
-  for(int e=0;i<3;i++){
-    digitalWrite(plantas[i], HIGH );
-    
-    
-    
+      almacenar[i]=almacenar[4];
+      
     }
   
-  }
+  }*/
