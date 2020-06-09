25c6d3216e22e62823dedd5465b2dfb81132738e
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-06-09 01:46:49 -0300

    Cerradura

---
+++ TPN°2/programa/programa.ino
@@ -0,0 +1,99 @@
+int x;
+
+void setup() {
+  Serial.begin (9600);
+  for(int i=2; i <=4; i++){
+    pinMode(i, OUTPUT);
+    }
+  for (int e=5;e<=8;e++){
+    pinMode(e,INPUT);
+    
+    }
+  Serial.println("menu");
+  Serial.println("1_N1+N2");
+  Serial.println("2_N1*N2");
+  Serial.println("3_N1-N2");
+
+    
+}
+
+void loop() {
+  teclado();
+
+}
+
+void teclado(){
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  if(digitalRead(5)==HIGH){
+    Serial.println("Numero:1"); 
+    x=1;
+    delay(250); 
+      }
+  if(digitalRead(6)==HIGH){
+    Serial.println("numero:4");
+    x=4;
+    delay(250);
+    }
+  if (digitalRead(7)==HIGH){
+    Serial.println("Numero:7"); 
+    x=7;
+    delay(250); 
+    }
+    if (digitalRead(8)==HIGH){
+    Serial.println("Numero:*"); 
+    x=20;
+    delay(250); 
+    }
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  if(digitalRead(5)==HIGH){
+    Serial.println("numero:2");
+    x=2;
+    delay(250);}
+    if(digitalRead(6)==HIGH){
+    Serial.println("numero:4");
+    x=4;
+    delay(250);
+    }
+    if(digitalRead(7)==HIGH){
+    Serial.println("numero:8");
+    x=8;
+    delay(250);
+    }
+    if(digitalRead(8)==HIGH){
+    Serial.println("numero:0");
+    x=0;
+    delay(250);
+    }
+digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  if(digitalRead(5)==HIGH){
+    Serial.println("numero:3");
+    x=3;
+    delay(250);}
+    if(digitalRead(6)==HIGH){
+    Serial.println("numero:6");
+    x=6;
+    delay(250);
+    }
+    if(digitalRead(7)==HIGH){
+    Serial.println("numero:9");
+    x=9;
+    delay(250);
+    }
+    if(digitalRead(8)==HIGH){
+    Serial.println("numero:#");
+    x=21;
+    delay(250);
+    }
+
+    
+  }    
+
+
+  
+  
